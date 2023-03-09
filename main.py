049d77313753d3c1baf6bf758c5523f707ef475e
cottierguillya <cottierguillya@educ.college-montreal.qc.ca>
2023-03-09 09:08:19 -0500

    Ajout de plusieurs éléments et formes ; il manque le triangle

--- main.py
+++ main.py
@@ -4,29 +4,27 @@ Fait le 18 janvier 2023
 Ce programme teste les dessins avec arcade en dessinant quelque chose
 que l'on peut reconnaitre avec différentes formes
 """
+import math
 import random
 
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Modèle de départ"
+SCREEN_WIDTH = 800 * 2
+SCREEN_HEIGHT = 600 * 2
+SCREEN_TITLE = "Les forêts magnifiques"
 
 COLORS = [arcade.color.ROCKET_METALLIC, arcade.color.DARK_BROWN, arcade.color.DARK_GREEN,
-          arcade.color.RED, arcade.color.YELLOW, arcade.color.BAKER_MILLER_PINK, arcade.color.SKY_BLUE]
+          arcade.color.RED, arcade.color.YELLOW, arcade.color.BAKER_MILLER_PINK, arcade.color.SKY_BLUE,
+          arcade.color.BLACK, arcade.color.WHITE]
 
 CIRCLE = 0
 SQUARE = 1
-TRIANGLE = 2
-ARC = 3
-LINE = 4
-POLYGON = 5
-ELLIPSE = 6
 
-SHAPES = [CIRCLE, SQUARE, TRIANGLE, ARC, LINE, POLYGON, ELLIPSE]
-SHAPE_NUMBER_KEY = [arcade.key.KEY_1, arcade.key.KEY_2, arcade.key.KEY_3,
-                    arcade.key.KEY_4, arcade.key.KEY_5, arcade.key.KEY_6, arcade.key.KEY_7]
-SHAPE_COLORS_KEY = SHAPE_NUMBER_KEY
+SHAPES = [CIRCLE, SQUARE]
+SHAPE_NUMBER_KEY = [arcade.key.KEY_1, arcade.key.KEY_2, arcade.key.KEY_3]
+SHAPE_COLORS_KEY = [arcade.key.KEY_1, arcade.key.KEY_2, arcade.key.KEY_3,
+                    arcade.key.KEY_4, arcade.key.KEY_5, arcade.key.KEY_6, arcade.key.KEY_7,
+                    arcade.key.KEY_8, arcade.key.KEY_9]
 
 
 class DessinMagnifique(arcade.Window):
@@ -44,7 +42,7 @@ class DessinMagnifique(arcade.Window):
         self.mouse_pressed = False
         self.mouse_position = -100, -100
         self.mouse_color = COLORS[0]
-        self.mouse_size = 1
+        self.mouse_size = 1.0
         self.shape = CIRCLE
         self.mouse_alpha = 255
         self.mouse_middle_click = False
@@ -86,29 +84,56 @@ class DessinMagnifique(arcade.Window):
     def load_new_terrain(self):
         self.map_seed = random.randint(1, 1_000_000_000_000_000)
 
-    def draw_tree(self, position, size):
+    @staticmethod
+    def draw_tree(position, size):
         arcade.draw_rectangle_filled(position[0], position[1], size[0], size[1], arcade.color.WOOD_BROWN)
         arcade.draw_ellipse_filled(position[0], position[1] + size[1] / 1.2, size[0] * random.uniform(2, 3),
                                    size[1] * random.uniform(1.5, 2.5), arcade.color.DARK_GREEN)
 
-    def draw_rock(self, position, size):
+    @staticmethod
+    def draw_rock(position, size):
         arcade.draw_circle_filled(position[0], position[1], size[0] / 2.5, num_segments=random.randint(5, 8), color=arcade.color.COOL_GREY)
-    def get_object_setup(self):
+
+    @staticmethod
+    def draw_grass(position, size):
+        arcade.draw_arc_outline(position[0], position[1], size[0] * 0.5, size[1] * 0.75, (0, 100, 0), 0, random.uniform(45, 130), SCREEN_WIDTH / 150)
+
+    @staticmethod
+    def get_object_setup():
         position = random.uniform(0, SCREEN_WIDTH), random.uniform(0, SCREEN_HEIGHT / 3)
         size = random.uniform(SCREEN_WIDTH / 50, SCREEN_WIDTH / 25), random.uniform(SCREEN_HEIGHT / 35,
                                                                                     SCREEN_HEIGHT / 15)
         return position, size
+
     def draw_background(self):
         arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 6,
                                      SCREEN_WIDTH, SCREEN_HEIGHT / 3, arcade.color.ARMY_GREEN)
         random.seed(self.map_seed)
+        for i in range(random.randint(25, 150)):
+            self.draw_grass(*self.get_object_setup())
+
         for i in range(random.randint(3, 5)):
             self.draw_rock(*self.get_object_setup())
 
         for i in range(random.randint(5, 20)):
             self.draw_tree(*self.get_object_setup())
 
-        nom_foret = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(random.randint(3, 7))]).capitalize()
+        sun_position = random.randint(round(SCREEN_WIDTH / 15), round(SCREEN_WIDTH / 15 * 14)), SCREEN_HEIGHT / 8 * 7
+
+        arcade.draw_circle_filled(sun_position[0], sun_position[1], SCREEN_WIDTH / 20, arcade.color.SUNGLOW)
+
+        number_of_radius = 10
+        for angle in range(number_of_radius):
+            # Dessiner les rayons du soleil
+            angle = 360 / number_of_radius * (angle + 1)
+            distance_from_center = SCREEN_WIDTH / 20
+            start_position = sun_position[0] + math.cos(math.radians(angle)) * distance_from_center, \
+                                sun_position[1] + math.sin(math.radians(angle)) * distance_from_center
+            end_position = sun_position[0] + math.cos(math.radians(angle)) * distance_from_center * 2, \
+                                sun_position[1] + math.sin(math.radians(angle)) * distance_from_center * 2
+            arcade.draw_line(start_position[0], start_position[1], end_position[0], end_position[1], arcade.color.SUNGLOW)
+
+        nom_foret = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(random.randint(3, 7))]).capitalize()
         arcade.draw_text(f'La forêt de {nom_foret}', 0, round(SCREEN_HEIGHT / 3 * 2),
                          arcade.color.MIDNIGHT_BLUE, width=SCREEN_WIDTH, bold=True,
                          font_size=round(SCREEN_WIDTH / 15), align='center', font_name='Garamond')
